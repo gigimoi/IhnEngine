689b681c20df7f1ddb83421ac606400dae835068
Gavin Gassmann <gigimoigames@gmail.com>
2013-09-01 19:33:45 -0500

    Fixed health
    Signed-off-by: Gavin Gassmann <gigimoigames@gmail.com>

--- IhnEngine/Components/ComponentHealth.cs
+++ IhnEngine/Components/ComponentHealth.cs
@@ -9,7 +9,11 @@ using System;
 
 namespace IhnLib {
 	[Serializable]
-	public class ComponentHealth {
+	public class ComponentHealth : Component{
+		public ComponentHealth(int maxHealth) {
+			MaxHealth = maxHealth;
+			Health = maxHealth;
+		}
 		public int Health;
 		public int MaxHealth;
 	}
