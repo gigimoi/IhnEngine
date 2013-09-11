87d9a4a74293a5cb51ef49281c3fc0e68c019cc2
Gavin Gassmann <gigimoigames@gmail.com>
2013-09-10 22:52:50 -0500

    Fixed GetPixel() and GetNone()
    Signed-off-by: Gavin Gassmann <gigimoigames@gmail.com>

--- IhnEngine/Art.cs
+++ IhnEngine/Art.cs
@@ -13,14 +13,14 @@ namespace IhnLib {
 	public static class Art {
 		static Texture2D _pixel;
 		static Texture2D _none;
-		public static Texture2D GetNone() {
+		public static Texture2D GetPixel() {
 			if(_pixel == null) {
 				_pixel = new Texture2D(Ihn.Instance.GraphicsDevice, 1, 1);
 				_pixel.SetData<Color>(new Color[] { Color.White });
 			}
 			return _pixel;
 		}
-		public static Texture2D GetPixel() {
+		public static Texture2D GetNone() {
 			if(_pixel == null) {
 				_pixel = new Texture2D(Ihn.Instance.GraphicsDevice, 1, 1);
 				_pixel.SetData<Color>(new Color[] { new Color(0, 0, 0, 0) });
