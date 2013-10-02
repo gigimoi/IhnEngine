179c8ccd69acce317fd97c539ad441e2d2fb4d9c
Gavin Gassmann <gassmann.gavin@gmail.com>
2013-10-02 00:51:25 -0500

    Added null check to SystemRenderMultisprite.Render() Added remove function to tile editor Removed debug output from tile editor Tiles can now be loaded from an INI file
    Signed-off-by: Gavin Gassmann <gassmann.gavin@gmail.com>

--- IhnEngine/Systems/SystemRenderMultisprite.cs
+++ IhnEngine/Systems/SystemRenderMultisprite.cs
@@ -18,12 +18,14 @@ namespace IhnLib {
 			var pos = entity.GetComp<ComponentPosition>();
 			var sprite = entity.GetComp<ComponentMultiSprite>();
 			for(int i = 0; i < sprite.Textures.Count; i++) {
-				if(entity.HasComp<ComponentSize>()) {
-					var size = entity.GetComp<ComponentSize>();
-					spriteBatch.Draw(Rsc.Load<Texture2D>(sprite.Textures[i]), new Rectangle((int)pos.X - (int)ihn.CameraPos.X, (int)pos.Y - (int)ihn.CameraPos.Y, size.Width, size.Height), sprite.Source.ToRect(), Color.White, sprite.Rotation, sprite.Origin, sprite.Mirror, 0);
-				}
-				else {
-					spriteBatch.Draw(Rsc.Load<Texture2D>(sprite.Textures[i]), new Rectangle((int)pos.X - (int)ihn.CameraPos.X, (int)pos.Y - (int)ihn.CameraPos.Y, (int)sprite.Source.Width, (int)sprite.Source.Height), sprite.Source.ToRect(), Color.White, sprite.Rotation, sprite.Origin, sprite.Mirror, 0);
+				if(sprite.Textures[i] != null) {
+					if(entity.HasComp<ComponentSize>()) {
+						var size = entity.GetComp<ComponentSize>();
+						spriteBatch.Draw(Rsc.Load<Texture2D>(sprite.Textures[i]), new Rectangle((int)pos.X - (int)ihn.CameraPos.X, (int)pos.Y - (int)ihn.CameraPos.Y, size.Width, size.Height), sprite.Source.ToRect(), Color.White, sprite.Rotation, sprite.Origin, sprite.Mirror, 0);
+					}
+					else {
+						spriteBatch.Draw(Rsc.Load<Texture2D>(sprite.Textures[i]), new Rectangle((int)pos.X - (int)ihn.CameraPos.X, (int)pos.Y - (int)ihn.CameraPos.Y, (int)sprite.Source.Width, (int)sprite.Source.Height), sprite.Source.ToRect(), Color.White, sprite.Rotation, sprite.Origin, sprite.Mirror, 0);
+					}
 				}
 			}
 		}
